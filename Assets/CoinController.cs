2f8589d06b824743666ffbdbede699abc9a372be
Gabriel Bezerra <rhuanantunes2007@gmail.com>
2026-06-26 16:03:14 -0300

    stiches

--- Assets/CoinController.cs
+++ Assets/CoinController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    public GameObject coinParticle;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Player")){
+            Instantiate(coinParticle,
+            this.transform.position,
+            Quaternion.Euler(-90, 0, 0));
         Destroy(this.gameObject);
         }
     }
